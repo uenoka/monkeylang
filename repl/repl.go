1a8249c6028c35ee91fe34820cb654bf4c2145fd
kazukiueno <ueno@binc.jp>
2022-04-29 21:46:38 +0900

    add repl.go

---
+++ repl/repl.go
@@ -0,0 +1,25 @@
+package repl
+import(
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer){
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned = scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.new(line)
+		for tok := l.NextToken(); tok.Type != token.EOF;tok = l.NextToken(){
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
